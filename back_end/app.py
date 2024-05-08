8ad58e1015c4724c0b2a07a31ed544ab785855a3
Fra <fra24eso@gmail.com>
2024-05-08 17:03:54 +0200

    app.py: add route /login

--- back_end/app.py
+++ back_end/app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('home/home.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('home/user_access/login.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=environ.get('PORT'),debug=environ.get('DEBUG'))
